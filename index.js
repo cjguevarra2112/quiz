5c85c78a3544693c7cb7f3c979841cc799b50c9c
cjguevarra2112 <cjguevarra2112@yahoo.com>
2016-05-17 15:13:15 +0800

    Created render_question method for rendering question to HTML

--- index.js
+++ index.js
@@ -47,24 +47,70 @@ let theQuestions = [
     }
 ];
 
-var currentAnswer = null;
-var currentStep = null;
 
-var quizForm = document.querySelector("#quiz-form");
-var submitButtons = document.querySelectorAll(".submit");
+let currentIndex = 0;
 
-quizForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let choice = event.target.choice.value;
-    currentAnswer = choice;
+init();
+onQuestionSubmit();
+
+function init() {
+    renderQuestion(theQuestions[0], currentIndex);
+}
+
+function renderQuestion(question, index) {
     
-    alert(currentAnswer);
-    alert(currentStep);
-});
-
-Array.prototype.forEach.call(submitButtons, (button) => {
-    button.addEventListener("click", (event) => {
-        let step = event.target.value;
-        currentStep = step;
+    // Cache DOM
+    let questionText = document.querySelector("#question-text");
+    let questionNumber = document.querySelector("#question-number");
+    let choices = document.querySelector("#choices");
+    
+    // Render question text
+    questionText.textContent = question.text;
+    // Render question number
+    questionNumber.textContent = (currentIndex + 1).toString();
+    
+    // Render choices
+    question.choices.forEach((choice) => {
+        let choiceParagraph = document.createElement("p");
+        let choiceRadio = document.createElement("input");
+        let choiceText = document.createTextNode(choice);
+        
+        choiceRadio.type = "radio";
+        choiceRadio.name = "choice";
+        choiceRadio.value = choice;
+        choiceParagraph.appendChild(choiceRadio);
+        choiceParagraph.appendChild(choiceText);
+        
+        choices.appendChild(choiceParagraph);
     });
-});
+    
+}
+
+
+
+function onQuestionSubmit() {
+    var currentAnswer = null; // current choice to the current question
+    var currentStep = null;   // current step (next/previous)
+    
+    // The question form
+    var quizForm = document.querySelector("#quiz-form");
+    // Either of previous or next button
+    var submitButtons = document.querySelectorAll(".step");
+
+    quizForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let choice = event.target.choice.value;
+        currentAnswer = choice;
+        
+        alert(currentAnswer);
+        alert(currentStep);
+    });
+
+    Array.prototype.forEach.call(submitButtons, (button) => {
+        button.addEventListener("click", (event) => {
+            let step = event.target.value;
+            currentStep = step;
+        });
+    });
+}
+
